8ec33eb4abebd33b57ef74b967b737675120e9a0
Colin Cook <cook.colin13@gmail.com>
2021-07-21 07:21:22 -0500

    adding depencies

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[tokio::main]
+
+async fn main() {
+    get_html().await;
+}
+
+async fn get_html() -> Result<(), Box<dyn std::error::Error>> {
+    let html = reqwest::get("https://stacoverflow.com/search?q=rust").await?;
+    println!("{:#?}", html);
+    Ok(())
+}
