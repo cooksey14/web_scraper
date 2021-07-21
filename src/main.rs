2a58f434ad3bb318cfc56e3a9b793369b0764aa3
Colin Cook <cook.colin13@gmail.com>
2021-07-21 08:16:55 -0500

    scraping stackoverflow

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+extern crate select;
+use select::document::Document;
+use select::predicate::Name;
+use std::io::Cursor;
 #[tokio::main]
 
 async fn main() {
-   let html_data = get_html().await;
-   println!("{:#?}", html_data);
+    let html_data:std::string::String = get_html().await.unwrap();
+    //println!("{:#?}", html_data);
+    let cursor:std::io::Cursor<std::string::String> = Cursor::new(html_data);
+    //println!("{:#?}", cursor);
+    let document = Document::from_read(cursor);
+    //println!("{:#?}", document);
+    let found = document
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+    //println!("{}", found);
 }
 
 async fn get_html() -> Result<std::string::String, Box<dyn std::error::Error>> {
-    let html = reqwest::get("https://stacoverflow.com/search?q=rust").await?.text().await?;
+    let html = reqwest::get("https://stackoverflow.com/search?q=rust")
+        .await?
+        .text()
+        .await?;
     //println!("{:#?}", html);
     Ok(html)
 }
