8e9df8643a0da7f85ddd981d98cd166a1186853b
Colin Cook <cook.colin13@gmail.com>
2021-07-21 07:27:05 -0500

    printing html_data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #[tokio::main]
 
 async fn main() {
-    get_html().await;
+   let html_data = get_html().await;
+   println!("{:#?}", html_data);
 }
 
-async fn get_html() -> Result<(), Box<dyn std::error::Error>> {
+async fn get_html() -> Result<(std::string::String), Box<dyn std::error::Error>> {
     let html = reqwest::get("https://stacoverflow.com/search?q=rust").await?.text().await?;
-    println!("{:#?}", html);
-    Ok(())
+    //println!("{:#?}", html);
+    Ok((html))
 }
